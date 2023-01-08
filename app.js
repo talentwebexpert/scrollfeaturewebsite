30832b2b79174b3c551afa0a95e49b456679bc87
talentwebexpert <121409036+talentwebexpert@users.noreply.github.com>
2023-01-08 13:22:11 -0800

    Day 14 of 100daysofcode Responsive Scroll website

---
+++ app.js
@@ -0,0 +1,30 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+// ********** close links ************
+const linksContainer = document.querySelector(".links-container");
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    linksContainer.classList.toggle("show-links")
+    const linksHeight = String(Math.floor(links.getBoundingClientRect().height)) + "px";
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linksContainer.style.height = linksHeight
+    } else{
+        linksContainer.style.height = 0
+
+    }
+})
+// ********** fixed navbar ************
+window.addEventListener("scroll", function(){
+    console.log(window.pageYOffset)
+})
+// ********** smooth scroll ************
+// select links
