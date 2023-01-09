fc282534b50cd0a7823f3374b3eba4bf76d77c9a
talentwebexpert <121409036+talentwebexpert@users.noreply.github.com>
2023-01-09 14:35:28 -0800

    Day 15 of 100daysofcode
    Add feature to day 14 website

--- app.js
+++ app.js
@@ -19,12 +19,44 @@ navToggle.addEventListener("click", function(){
         linksContainer.style.height = linksHeight
     } else{
         linksContainer.style.height = 0
-
     }
 })
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
 window.addEventListener("scroll", function(){
-    console.log(window.pageYOffset)
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add("fixed-nav")
+    } else{
+        navbar.classList.remove("fixed-nav")
+    }
+    if(scrollHeight > navHeight + navHeight){
+        topLink.classList.add('show-link')
+    } else{
+        topLink.classList.remove('show-link')
+    }
 })
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link){
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id)
+        const navHeight = navbar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+
+        let position = element.offsetTop - navHeight
+        console.log(position)
+        window.scrollTo({
+            left: 0, top: position
+        })
+        linksContainer.style.height = 0;
+    })
+})
